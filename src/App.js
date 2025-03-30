562c2ae2bdecf5e5a584db683e8a7545dcd1f89e
Ghanshree Mahure <93911427+ghanshree2311@users.noreply.github.com>
2025-03-30 19:10:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import BlogList from './components/BlogList';
 import BlogDetail from './components/BlogDetail';
-import Login from './components/Login'; // Import the Login component
-import ProtectedRoute from './components/ProtectedRoute'; // Import the ProtectedRoute component
-import { AuthProvider } from './context/AuthContext'; // Correct path to AuthContext
+import Login from './components/Login'; 
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
 import './App.css';
 import Navbar from './components/Navbar';
-import './index.css'; // Ensure this line is present
-import Home from './components/Home'; // Adjust the path if necessary
+import './index.css';
+import Home from './components/Home';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
